00cb21de0a823ee9de32d3be12616b888a2a9e9d
G-DOC69 <erlan.turar.5@gmail.com>
2023-12-11 20:14:27 +0600

    contextprovider + minor changes

--- edilgpt/src/App.jsx
+++ edilgpt/src/App.jsx
@@ -5,13 +5,24 @@ import SignUpPage from '../src/pages/SignUpPage'
 import MainRoutes from './routes/routes'
 import { BrowserRouter } from 'react-router-dom'
 // import Routes from '../src/routes/routes'
+import { useState, createContext } from 'react'
+
+export const UserContext = createContext('user');
 
 function App() {
+  const [user, setUser] = useState({
+    username: 'username',
+    firstname: 'First name',
+    secondname: 'Second Name'
+  })
+
   return (
     <>
-    <BrowserRouter>
-      <MainRoutes/>
-    </BrowserRouter>
+      <BrowserRouter>
+        <UserContext.Provider value={[user, setUser]}>
+          <MainRoutes/>
+        </UserContext.Provider>
+      </BrowserRouter>
     </>
   )
 }
--- edilgpt/src/components/Cabinet.jsx
+++ edilgpt/src/components/Cabinet.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import '../styles/CabinetStyle.scss'
 import { Icon } from '@iconify/react';
+import { useContext } from 'react';
+import { UserContext } from '../App';
 
 const Cabinet = () => {
     let navigate = useNavigate()
+    const [user, setUser] = useContext(UserContext);
+    
   return (
     <div id='cabinet_container'>
       <div id='profile_out'>
         <img id='profile_picture' src="../src/assets/user-placeholder.png" alt="user.png" width={120} height={120}/>
-        <p id='profile_name'>Username</p>
+        <p id='profile_name'>{user.username}</p>
         <button id='log_out_button' onClick={()=>navigate('/signup')}><Icon id='log_out_icon' icon="ic:sharp-log-out" /></button>
       </div>
       <div id='data_container'>
         <div id='info_container'>
             <h2 id='info_header'>Account Information</h2>
             <ul className='info_list'>
-                <li className='info_element' placeholder='First Name'>First Name</li>
-                <li className='info_element' placeholder='Second Name'>Second Name</li>
+                <li className='info_element' placeholder='First Name'>{user.firstname}</li>
+                <li className='info_element' placeholder='Second Name'>{user.secondname}</li>
                 <li className='info_element' placeholder='Date of Birth'>Date of Birth</li>
             </ul>
             <ul className='info_list'>
